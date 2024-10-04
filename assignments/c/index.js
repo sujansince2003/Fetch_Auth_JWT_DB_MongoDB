a7d86f9bc490d1bfccd06e4f57ba5a67bc293833
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-10-04 23:20:25 +0545

    setup entry point and main server

--- assignments/c/index.js
+++ assignments/c/index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const { connectDB } = require("./db");
+const { connectDB } = require("./db/db");
+const userRouter = require("./routes/user_route");
+const adminRouter = require("./routes/admin_route");
 dotenv.config({
   path: ".env",
 });
 
 //setting up express
 const app = express();
+app.use(express.json());
+
+app.use("/admin", adminRouter);
+app.use("/user", userRouter);
 
 async function startServer() {
   try {
-    connectDB();
+    await connectDB();
     app.listen(3000, () => {
       console.log("server started with database");
     });
