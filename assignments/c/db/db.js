f3b552a9cb38a79ab914d3c6c0798b5b932b37d0
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-10-04 16:23:29 +0545

    connecting to database,defining schema and models for Admin,courses and user

---
+++ assignments/c/db/db.js
@@ -0,0 +1,76 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({
+  path: ".env",
+});
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database connected::");
+  } catch (error) {
+    console.log("something went wrong while connecting to database");
+    throw error;
+  }
+}
+// defining schemas
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  purchasedcourse: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  ],
+});
+const adminSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+const courseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+  },
+});
+
+// defining model
+const User = mongoose.model("User", userSchema);
+const Admin = mongoose.model("Admin", adminSchema);
+const Course = mongoose.model("Course", courseSchema);
+
+// exporting the modules
+module.exports = {
+  connectDB,
+  User,
+  Admin,
+  Course,
+};
